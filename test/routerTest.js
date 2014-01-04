6ff1198ecc60e8ceaf365ecc651c1540b90da6ca
odino <alessandro.nadalin@gmail.com>
2014-01-04 16:42:23 +0400

    implemented routes loading via #load() and from a YML file with #loadFromFile() updated dependencies added a few more tests

--- test/routerTest.js
+++ test/routerTest.js
@@ -1,7 +1,73 @@
 var router = require('../lib/router');
 var should = require('should');
 
+var reset = function() {
+    router.routes = {};
+}
+
 describe('router', function(){
+    beforeEach(function(done){
+        router.routes = {}
+
+        done();
+    }),
+    describe('#load()', function(){
+        it('should load routes in config', function(){
+            router.load({ example: { pattern: '/example' }});
+            router.routes.should.be.eql({ example: { pattern: '/example' } });
+        }),
+        it('should merge routes if called multiple times', function(){
+            router.load({ example: { pattern: '/example' }});
+            router.load({ example2: { pattern: '/hello' }});
+            router.routes.should.be.eql({ example: { pattern: '/example' }, example2: { pattern: '/hello' }});
+        })
+        it('should throw an error if trying to load a route without pattern', function(){
+            try {
+                router.load({ example: {}});
+            } catch (err) {
+                err.message.should.be.eql('route "example" is missing the "pattern" attribute');
+
+                return;
+            }
+
+            throw new Error('should have thrown an error');
+        }),
+        it('should throw an error if trying to load a route and the pattern is an array', function(){
+            try {
+                router.load({ example: { pattern: [] }});
+            } catch (err) {
+                err.message.should.be.eql('the pattern of the route "example" must be a string (found [])');
+
+                return;
+            }
+
+            throw new Error('should have thrown an error');
+        }),
+        it('should throw an error if trying to load a route and the pattern is an object', function(){
+            try {
+                router.load({ example: { pattern: {a: 'b'} }});
+            } catch (err) {
+                err.message.should.be.eql('the pattern of the route "example" must be a string (found {"a":"b"})');
+
+                return;
+            }
+
+            throw new Error('should have thrown an error');
+        })
+    }),
+    describe('#loadFromFile()', function(){
+        it('should load routes in config from a YAML file', function(){
+            router.routes = {};
+            router.loadFromFile('./test/stub/ok_routes.yml');
+            router.routes.should.be.eql({ example: { pattern: '/example' } });
+        }),
+        it('should not load routes in config from an empty YAML file', function(){
+            router.routes = {};
+
+            router.loadFromFile('./test/stub/empty_routes.yml');
+            router.routes.should.be.eql({});
+        })
+    }),
     describe('#imatch()', function(){
         it('should match a raw path', function(){
             var exampleRoute = {
